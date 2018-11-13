4a53b76f55bda6c788d2d94f6b0c84f4ad734f57
Nyall Dawson <nyall.dawson@gmail.com>
2018-11-13 10:08:38 +1000

    GuiUtils class

---
+++ r/r_plugin.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""QGIS Processing R Provider
+
+.. note:: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+
+__author__ = '(C) 2018 by Nyall Dawson'
+__date__ = '20/10/2018'
+__copyright__ = 'Copyright 2018, North Road'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import os
+from qgis.PyQt.QtCore import (QSettings,
+                              QTranslator,
+                              QCoreApplication)
+from qgis.core import QgsApplication
+from qgis.gui import QgisInterface
+from r.processing.provider import RAlgorithmProvider
+VERSION = '0.1'
+
+
+class RProviderPlugin:
+    """QGIS Plugin Implementation."""
+
+    def __init__(self, iface: QgisInterface):
+        """Constructor.
+
+        :param iface: An interface instance that will be passed to this class
+            which provides the hook by which you can manipulate the QGIS
+            application at run time.
+        :type iface: QgsInterface
+        """
+        super().__init__()
+        # Save reference to the QGIS interface
+        self.iface = iface
+        # initialize plugin directory
+        self.plugin_dir = os.path.dirname(__file__)
+        # initialize locale
+        locale = QSettings().value('locale/userLocale')[0:2]
+        locale_path = os.path.join(
+            self.plugin_dir,
+            'i18n',
+            '{}.qm'.format(locale))
+
+        if os.path.exists(locale_path):
+            self.translator = QTranslator()
+            self.translator.load(locale_path)
+            QCoreApplication.installTranslator(self.translator)
+
+        # processing framework
+        self.provider = RAlgorithmProvider()
+
+    @staticmethod
+    def tr(message):
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('RProvider', message)
+
+    def initGui(self):
+        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+        QgsApplication.processingRegistry().addProvider(self.provider)
+
+    def unload(self):
+        """Removes the plugin menu item and icon from QGIS GUI."""
+        QgsApplication.processingRegistry().removeProvider(self.provider)
