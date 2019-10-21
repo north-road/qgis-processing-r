e8375981ceff7f6a290acc9e8e0212fd35eca168
Nyall Dawson <nyall.dawson@gmail.com>
2019-10-21 17:01:45 +1000

    Fix more non-toplevel imports

--- processing_r/__init__.py
+++ processing_r/__init__.py
@@ -10,6 +10,8 @@
  ***************************************************************************/
 """
 
+from .r_plugin import RProviderPlugin
+
 
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
@@ -19,5 +21,4 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .r_plugin import RProviderPlugin
     return RProviderPlugin(iface)
