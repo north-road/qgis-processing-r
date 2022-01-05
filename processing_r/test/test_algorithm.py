3d68d0150df514d37d7249dee6a98d9b79dd3a6b
Jan Caha <jan.caha@outlook.com>
2022-01-05 18:20:28 +0100

    better test skipping

--- processing_r/test/test_algorithm.py
+++ processing_r/test/test_algorithm.py
@@ -558,11 +558,13 @@ class AlgorithmTest(unittest.TestCase):
         script = alg.build_r_script({'range': [0.5, 1.5]}, context, feedback)
         self.assertIn('range <- c(min = 0.5, max = 1.5)', script)
 
-    @unittest.skipIf(Qgis.QGIS_VERSION_INT < 31000, "QGIS version does not support this.")
     def testAlgColorInput(self):
         """
         Test color parameter
         """
+
+        if Qgis.QGIS_VERSION_INT < 31000:
+            self.skipTest("QGIS version does not support this.")
 
         context = QgsProcessingContext()
         feedback = QgsProcessingFeedback()
@@ -575,11 +577,13 @@ class AlgorithmTest(unittest.TestCase):
         script = alg.build_r_script({'color': QColor(255, 0, 0)}, context, feedback)
         self.assertIn('color <- rgb(255, 0, 0, 255, maxColorValue = 255)', script)
 
-    @unittest.skipIf(Qgis.QGIS_VERSION_INT < 31400, "QGIS version does not support this.")
     def testAlgDateTimeInput(self):
         """
         Test datetime parameter
         """
+
+        if Qgis.QGIS_VERSION_INT < 31400:
+            self.skipTest("QGIS version does not support this.")
 
         context = QgsProcessingContext()
         feedback = QgsProcessingFeedback()
