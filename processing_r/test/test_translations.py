e42f9ffc956be5d59851d6f762888866eba754a5
Jan Caha <jan.caha@outlook.com>
2022-01-28 14:09:07 +0100

    fix pylint

--- processing_r/test/test_translations.py
+++ processing_r/test/test_translations.py
@@ -26,12 +26,12 @@ class SafeTranslationsTest(unittest.TestCase):
 
     def setUp(self):
         """Runs before each test."""
-        if 'LANG' in iter(os.environ.keys()):
+        if 'LANG' in iter(os.environ.keys()):  # pylint: disable=consider-iterating-dictionary
             os.environ.__delitem__('LANG')
 
     def tearDown(self):
         """Runs after each test."""
-        if 'LANG' in iter(os.environ.keys()):
+        if 'LANG' in iter(os.environ.keys()):  # pylint: disable=consider-iterating-dictionary
             os.environ.__delitem__('LANG')
 
     def test_qgis_translations(self):
