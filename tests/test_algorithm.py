6f39e68beda13de53f57d9b39c7b358f1a27c6f4
Jan Caha <jan.caha@outlook.com>
2023-12-03 15:11:17 +0100

    add test that runs

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1,3 +1,4 @@
+from qgis.core import QgsProcessingContext, QgsProcessingFeedback
 from utils import script_path
 
 from processing_r.processing.algorithm import RAlgorithm
@@ -8,3 +9,15 @@ def test_can_run():
     alg.initAlgorithm()
 
     assert alg.canExecute()
+
+
+def test_process():
+    alg = RAlgorithm(description_file=script_path("test_input_point.rsx"))
+    alg.initAlgorithm()
+
+    context = QgsProcessingContext()
+    feedback = QgsProcessingFeedback()
+
+    result = alg.processAlgorithm({"point": "20.219926,49.138354 [EPSG:4326]"}, context, feedback)
+
+    assert result == {}
