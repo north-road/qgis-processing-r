75e95fa713073346988f999e46c8a7498254ea32
Jan Caha <jan.caha@outlook.com>
2023-12-04 10:30:07 +0100

    print settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,6 +28,8 @@ def plugin_provider(setup_plugin, qgis_processing) -> RAlgorithmProvider:
 
     QgsApplication.processingRegistry().addProvider(provider)
 
+    print(ProcessingConfig.getSetting(RUtils.RSCRIPTS_FOLDER, True))
+
     test_scripts_path = Path(__file__).parent / "scripts"
     ProcessingConfig.setSettingValue(RUtils.RSCRIPTS_FOLDER, test_scripts_path.as_posix())
 
